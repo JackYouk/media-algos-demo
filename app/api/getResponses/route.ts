0cd03375cda4760c2b2ae60465424f39a1655288
Jack Youkstetter <jackyoukstetter1@gmail.com>
2024-10-16 21:05:02 -0700

    cache fix

--- app/api/getResponses/route.ts
+++ app/api/getResponses/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
+export const dynamic = "force-dynamic"
+export const fetchCache = 'force-no-store';
+
 export async function GET(req: NextRequest) {
     const { rows } = await sql`SELECT * FROM Responses;`;
     return NextResponse.json(
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ export const metadata: Metadata = {
   title: "CRWN1 - Media Algos Demo",
   description: "Interactive demo for the algorithms in media presentation.",
 };
-export const fetchCache = 'force-no-store';
 
 export default function RootLayout({
   children,
